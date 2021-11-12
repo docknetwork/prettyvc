dbf9029e8de9b944e0cef0332e717f2cb7fa5481
SamHellawell <3194321+SamHellawell@users.noreply.github.com>
2021-11-12 00:10:12 +0000

    Deploying to gh-pages from  @ 4775389b7be76dc07ac03408b4300d38a942f5b5 🚀

---
+++ _next/static/EwAYYViwdIiqk0or-yPUs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/815-6f17644f5c8b77bf.js","static/css/e8558ade95f4202a.css","static/chunks/pages/index-a54bac5315589da6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/Npu6c2ZQJJv4cFDLyTEMs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/815-6f17644f5c8b77bf.js","static/css/e8558ade95f4202a.css","static/chunks/pages/index-f48f3d1710be7f5d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
