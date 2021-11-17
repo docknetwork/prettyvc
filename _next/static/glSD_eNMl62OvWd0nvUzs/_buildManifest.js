878d66f4e0cd674ce718d42e2423a1cdbd1c8807
SamHellawell <3194321+SamHellawell@users.noreply.github.com>
2021-11-17 05:28:27 +0000

    Deploying to gh-pages from  @ 9a88e230fd81777642be40a4287d64a213ced30d 🚀

---
+++ _next/static/glSD_eNMl62OvWd0nvUzs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/234-bee428f911bada88.js","static/css/83e115366d80b0d1.css","static/chunks/pages/index-6a07434d5e63ffec.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
